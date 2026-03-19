46b6e4be953549d790aa4e976890a627ec6bc146
soma-therapie <therapie.pro84@gmail.com>
2026-03-19 20:58:27 +0100

    Create sw.js

---
+++ sw.js
@@ -0,0 +1,13 @@
+self.addEventListener('install', e => {
+  e.waitUntil(
+    caches.open('soma-v1').then(cache =>
+      cache.addAll(['/', '/index.html'])
+    )
+  );
+});
+
+self.addEventListener('fetch', e => {
+  e.respondWith(
+    caches.match(e.request).then(r => r || fetch(e.request))
+  );
+});
