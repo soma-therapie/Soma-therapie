f0c177e1137def2f30356cdb844b95e0be7d6813
soma-therapie <therapie.pro84@gmail.com>
2026-03-19 21:30:12 +0100

    fix cache

--- sw.js
+++ sw.js
@@ -1,13 +1,15 @@
-self.addEventListener('install', e => {
-  e.waitUntil(
-    caches.open('soma-v1').then(cache =>
-      cache.addAll(['/', '/index.html'])
-    )
-  );
+self.addEventListener(‘install’, e => {
+self.skipWaiting();
 });
 
-self.addEventListener('fetch', e => {
-  e.respondWith(
-    caches.match(e.request).then(r => r || fetch(e.request))
-  );
+self.addEventListener(‘activate’, e => {
+e.waitUntil(
+caches.keys().then(keys =>
+Promise.all(keys.map(key => caches.delete(key)))
+)
+);
+});
+
+self.addEventListener(‘fetch’, e => {
+e.respondWith(fetch(e.request).catch(() => caches.match(e.request)));
 });
